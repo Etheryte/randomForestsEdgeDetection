6a46f7a960c63593f192e4b38b721feba50d9455
eth <eth@ethbook.local>
2015-10-23 19:00:08 +0300

    Initial Commit

---
+++ randomForestsEdgeDetection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  randomForestsEdgeDetection
+//
+//  Created by eth on 23/10/15.
+//  Copyright (c) 2015 eth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
